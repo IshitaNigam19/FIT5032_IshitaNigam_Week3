1d4cef0744d66903dfcd2021cf99995011825d0d
inig0001 <inig0001@MV2TDCSTUL05007>
2023-08-20 19:04:15 +1000

    Add project files.

---
+++ FIT5032_MyCodeSnippet_1/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using FIT5032_MyCodeSnippet_1.Models.Exercise;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FIT5032_MyCodeSnippet_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "FIT5032";
+
+            HelloWorld.Hello hello = new HelloWorld.Hello();
+            ViewBag.Message = hello.GetHello();
+
+            Models.Exercise.ExampleDictionary ed = new ExampleDictionary();
+            ed.Example();
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
